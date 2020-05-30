cd9da97b05fae1f0b1d3d94ffc6d5fdf32bfbe12
Udeni dilrukshi <40499515+udenidilru@users.noreply.github.com>
2020-05-30 11:08:56 +0530

    created login interface

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -2,7 +2,14 @@ import React from 'react';
 import {
   StyleSheet,View,Text, ActivityIndicator} from 'react-native';
 
+import * as firebase from 'firebase'
+
 export default class LoadingScreen extends React.Component {
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user => {
+            this.props.navigation.navigate(user ? "App" : "Auth");
+        });
+    }
     render(){
         return (
             <View style={styles.container}>
